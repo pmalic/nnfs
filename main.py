cefe815382facb4e1cd34c77c7b9d93352dcfb1e
Predrag Malicevic <predrag@malicevic.net>
2023-10-24 00:07:26 +0200

    Chapter 3 final, page 71

--- main.py
+++ main.py
@@ -1,29 +1,26 @@
 #!/usr/bin/env python
 
 import numpy as np
+import nnfs
+from nnfs.datasets import spiral_data
+import matplotlib.pyplot as plt
 
-inputs = np.array([
-    [1.0, 2.0, 3.0, 2.5],
-	[2.0, 5.0, -1.0, 2.0],
-	[-1.5, 2.7, 3.3, -0.8]
-])
+nnfs.init()
 
-weights = np.array([
-    [0.2, 0.8, -0.5, 1],
-	[0.5, -0.91, 0.26, -0.5],
-	[-0.26, -0.27, 0.17, 0.87]
-])
-biases = [2.0, 3.0, 0.5]
+class Layer_Dense:
 
-weights2 = np.array([
-    [0.1, -0.14, 0.5],
-	[-0.5, 0.12, -0.33],
-	[-0.44, 0.73, -0.13]
-])
-biases2 = [-1, 2, -0.5]
+	def __init__ (self, n_inputs, n_neurons):
+		self.weights = 0.01 * np.random.randn(n_inputs, n_neurons)
+		self.biases = np.zeros((1, n_neurons))
+
+	def forward (self, inputs):
+		self.output = np.dot(inputs, self.weights) + self.biases
 
 
-layer1_outputs = np.dot(inputs, weights.T) + biases
-layer2_outputs = np.dot(layer1_outputs, weights2.T) + biases2
+X, y = spiral_data(samples = 100, classes = 3)
 
-print(layer2_outputs)
+dense1 = Layer_Dense(2, 3)
+
+dense1.forward(X)
+
+print(dense1.output[:5])
