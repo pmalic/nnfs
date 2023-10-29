da84c7294ea2c7a36e3ee55fd0fb9fc06126ed06
Predrag Malicevic <predrag@malicevic.net>
2023-10-30 00:06:25 +0100

    Chapter 6 final, page 138

--- main.py
+++ main.py
@@ -16,10 +16,12 @@ class Layer_Dense:
 	def forward (self, inputs):
 		self.output = np.dot(inputs, self.weights) + self.biases
 
+
 class Activation_ReLU:
 
 	def forward (self, inputs):
 		self.output = np.maximum(0, inputs)
+
 
 class Activation_Softmax:
 
@@ -32,6 +34,39 @@ class Activation_Softmax:
 		self.output = probabilities
 
 
+class Loss:
+
+	def calculate (self, output, y):
+
+		sample_losses = self.forward(output, y)
+
+		data_loss = np.mean(sample_losses)
+
+		return data_loss
+
+
+class Loss_CategoricalCrossEntropy (Loss):
+
+	def forward (self, y_pred, y_true):
+
+		samples = len(y_pred)
+
+		y_pred_clipped = np.clip(y_pred, 1e-7, 1 - 1e-7)
+
+		if len(y_true.shape) == 1:
+
+			correct_confidences = y_pred_clipped[ range(samples), y_true ]
+
+		elif len(y_true.shape) == 2:
+
+			correct_confidences = np.sum(y_pred_clipped * y_true, axis = 1)
+
+		negative_log_likelihoods = - np.log(correct_confidences)
+
+		return negative_log_likelihoods
+
+
+# setup
 X, y = spiral_data(samples = 100, classes = 3)
 
 dense1 = Layer_Dense(2, 3)
@@ -40,6 +75,9 @@ activation1 = Activation_ReLU()
 dense2 = Layer_Dense(3, 3)
 activation2 = Activation_Softmax()
 
+loss_function = Loss_CategoricalCrossEntropy()
+
+# forward
 dense1.forward(X)
 activation1.forward(dense1.output)
 
@@ -47,3 +85,16 @@ dense2.forward(activation1.output)
 activation2.forward(dense2.output)
 
 print(activation2.output[:5])
+
+# loss
+loss = loss_function.calculate(activation2.output, y)
+print('loss:', loss)
+
+# predictions
+predictions = np.argmax(activation2.output, axis = 1)
+
+if len(y.shape) == 2:
+	y = np.argmax(y, axis = 1)
+
+accuracy = np.mean(predictions == y)
+print('accuracy:', accuracy)
